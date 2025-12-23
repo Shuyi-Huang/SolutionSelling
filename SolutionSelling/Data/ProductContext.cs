4a2768805a81d63de099c09770e88b2196868a80
Shuyi Huang <s360672@students.cdu.edu.au>
2025-12-23 12:33:07 +0930

    first commit

---
+++ SolutionSelling/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SolutionSelling.Models.Products;
+
+namespace SolutionSelling.Data
+{
+	public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+        }
+    }
+}
+
---
+++ SolutionSelling/Models/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SolutionSelling.Models.Products
+{
+	public class Product
+	{
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Photo { get; set; }
+    }
+}
+
